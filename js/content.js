65e913ac87bde3633d06da7cd05a8180a3655d13
Derek Antrican <deantric@microsoft.com>
2021-09-13 14:22:52 -0700

    Reworked the code to make it faster, simpler, & more stable

--- js/content.js
+++ js/content.js
@@ -1,122 +1,44 @@
-// Looking up the css background color is expensive, so cache the results
-var colorValueCache = {};
-function getColorValues(label) {
-  var classes = label.className;
-  if (!colorValueCache[classes]) {
-    var color = $(label).css("background-color");
-    colorValueCache[classes] = color.replace("rgb", "").replace("(", "").replace(")", "").split(",");
+'use strict';
 
-    colorValueCache[classes][0] = parseInt(colorValueCache[classes][0]);
-    colorValueCache[classes][1] = parseInt(colorValueCache[classes][1]);
-    colorValueCache[classes][2] = parseInt(colorValueCache[classes][2]);
-  }
-
-  return colorValueCache[classes];
-}
-
-function colorizeCards($cards) {
-  $cards.each(function (i, card) {
-    var $card = $(card);
-    var $labels = $card.find('span.card-label');
-
-    var labelPriority = ['Feature', 'Bug'];
-    var $labelToUse = null;
-
-    for (var i = 0; i < labelPriority.length; i++){
-      for (var j = 0; j < $labels.length; j++){
-        if ($labels[j].title == labelPriority[i]){
-          $labelToUse = $labels[j];
-          break;
-        }
-      }
-
-      if ($labelToUse != null)
-        break;
-    }
-
-    if ($labelToUse == null)
-      return;
-
-    if ($labels.size()) {
-      var r = 0;
-      var g = 0;
-      var b = 0;
-
-      var colorArray = getColorValues($labelToUse);
-
-      r = colorArray[0];
-      g = colorArray[1];
-      b = colorArray[2];
-
-      // Averaging with #e3e3e3 to lighten the colour and make it appropriate for a background
-      r = (r + 227) / 2;
-      g = (g + 227) / 2;
-      b = (b + 227) / 2;
-
-      var rgb = 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
-
-      if ($card.css("background-color") !== rgb)
-        $card.css("background-color", rgb);
-
-      var $cardDetails = $card.find('.list-card-details');
-      if (!$cardDetails.data('cardColorsForTrello.initStyles')) {
-        $cardDetails.css("background-color", "transparent");
-
-        $cardDetails.data('cardColorsForTrello.initStyles', true);
-      }
-
-      var $stickers = $card.find('.sticker');
-      $.each($stickers, function(i, sticker){
-        var $sticker = $(sticker);
-        var stickerOpacity = parseInt($sticker.css("top")) > 5 ? ".4" : "1";
-
-        if ($sticker.data('cardColorsForTrello.opacity') !== stickerOpacity) {
-          $sticker.css("opacity", stickerOpacity);
-
-          $sticker.data('cardColorsForTrello.opacity', stickerOpacity);
-        }
-      });
-
-      var $badges = $card.find('.badges');
-      if (!$badges.data('cardColorsForTrello.initStyles')) {
-        $badges.css("background-color", "rgba(255,255,255,0.7)");
-        $badges.css("padding", "2px 2px 0 2px");
-        $badges.css("margin-bottom", "2px");
-        $badges.css("border-radius", "3px");
-
-        $badges.data('cardColorsForTrello.initStyles', true);
-      }
-
-      if ($labels.data('cardColorsForTrello.hidden')) {
-        $labels.show();
-
-        $labels.data('cardColorsForTrello.hidden', false);
-      }
-    } else {
-      if ($card.data('cardColorsForTrello.bgColor')) {
-        $card.css("background-color", "");
-
-        $card.data('cardColorsForTrello.bgColor', null);
-      }
-    }
+function init(){
+  (new MutationObserver(function(wrappersTemplates){
+    wrappersTemplates.forEach(colorCards);
+  })).observe(document.body, {
+    childList : true,
+    subtree : true
   });
 }
 
-var iteration = 0;
-function colorize() {
-  // Only process "pirate-aged" cards every 10th iteration
-  // When a label is applied, the card should become de-pirated anyway and get processed immediately
-  if (iteration % 10 === 0) {
-    colorizeCards($('a.list-card'));
-  } else {
-    colorizeCards($('a.list-card:not(.aging-pirate), a.list-card.aging-level-0'));
-  }
+function colorCards(template) {
+    var element = template.target;
 
-  iteration++;
+    if (element.classList.contains("list-card-labels")) {
+        if (element.parentElement != null) {
+            assignColorFromLabels(element.parentElement, element.querySelectorAll("span.card-label"));
+        }
+    }
 
-  setTimeout(colorize, 500);
-};
+    if (element.classList.contains("list-card")) {
+        var cardElement = element.querySelector(".list-card-details");
+        if (cardElement != null) {
+            assignColorFromLabels(cardElement, element.querySelectorAll(".list-card-labels > span.card-label"));
+        }
+    }
+}
 
-$(document).ready(function() {
-  colorize();
-});
+function assignColorFromLabels(cardElement, labels){
+    if (labels != null){
+        var matchingLabels = Array.from(labels).filter(l => l.title == 'Bug' || l.title == 'Feature');
+        if (matchingLabels.length > 0){
+            var labelForColor = matchingLabels[0];
+            cardElement.style.background = "";
+            cardElement.style.backgroundColor = window.getComputedStyle(labelForColor).backgroundColor.replace("rgb", "rgba").replace(")", ",0.5)");
+        }
+        else{
+            cardElement.style.background = "";
+            cardElement.style.backgroundColor = "";
+        }
+    }
+}
+
+init();
